b8bdd7d9a9256cb4ce24fb4cecbbf514f8554fa4
evecus <jamie55@163.com>
2026-06-25 08:26:26 +0800

    Add files via upload

--- app/src/main/java/com/lizongying/mytv/speedtest/IptvSource.kt
+++ app/src/main/java/com/lizongying/mytv/speedtest/IptvSource.kt
@@ -13,10 +13,3 @@ data class IptvEntry(
     val speed: Double,      // MB/s，-1 表示失败
     val sourceIndex: Int,
 )
-
-data class ApiSourceResult(
-    val host: String,
-    val matchType: String,  // txiptv / jsmpeg / zhgxtv / hsmdtv
-    val speed: Double,
-    val channels: List<IptvChannel> = emptyList(),
-)
